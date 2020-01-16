d6b9fa2938084b617c543777cf1f2cad24d2b99a
Gerda Gatling <gerdagatling@Gerdas-MBP.home>
2020-01-15 21:35:33 -0500

    Added User Profile Model

---
+++ myfavmovie/src/main/java/com/example/myfavmovie/Models/UserProfile.java
@@ -0,0 +1,65 @@
+package com.example.myfavmovie.Models;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_profile")
+public class UserProfile {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String favorite_movie;
+
+    @Column
+    private String favorite_actor;
+
+    @Column
+    private String favorite_genre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFavorite_movie() {
+        return favorite_movie;
+    }
+
+    public void setFavorite_movie(String favorite_movie) {
+        this.favorite_movie = favorite_movie;
+    }
+
+    public String getFavorite_actor() {
+        return favorite_actor;
+    }
+
+    public void setFavorite_actor(String favorite_actor) {
+        this.favorite_actor = favorite_actor;
+    }
+
+    public String getFavorite_genre() {
+        return favorite_genre;
+    }
+
+    public void setFavorite_genre(String favorite_genre) {
+        this.favorite_genre = favorite_genre;
+    }
+}
