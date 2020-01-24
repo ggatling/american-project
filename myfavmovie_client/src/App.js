5e01ba81818a4972a21caf7d2da45e6c69ed1e9b
Gerda Gatling <gerdagatling@Gerdas-MBP.home>
2020-01-23 22:56:28 -0500

    added NavigationBar component

--- myfavmovie_client/src/App.js
+++ myfavmovie_client/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 // import Home from "./components/Home.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import { Layout } from "./components/Layout.js";
+import NavigationBar from "./components/NavigationBar.js";
 
 function App(){
   return (
     <React.Fragment>
-
+      <NavigationBar />
       <Router>
         <Switch>
 
