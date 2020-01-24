f8b9a0683f1add7c25fa3a4965e9dbcb1a24e9c1
Gerda Gatling <gerdagatling@Gerdas-MBP.home>
2020-01-24 00:08:31 -0500

    Addd jumbotron component

--- myfavmovie_client/src/App.js
+++ myfavmovie_client/src/App.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, {Component} from "react";
 // import Home from "./components/Home.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import { Layout } from "./components/Layout.js";
 import NavigationBar from "./components/NavigationBar.js";
+import { Jumbotron } from "./components/Jumbotron.js";
 
-function App(){
-  return (
-    <React.Fragment>
-      <NavigationBar />
-      <Router>
-        <Switch>
+class App extends Component{
+  render(){
+    return (
+      <React.Fragment>
+        <NavigationBar />
+        <Jumbotron />
+        <Router>
+          <Switch>
 
-        </Switch>
-      </Router>
+          </Switch>
+        </Router>
 
-  </React.Fragment>
-  );
+    </React.Fragment>
+    );
+  }
 }
+
 
 export default App;
