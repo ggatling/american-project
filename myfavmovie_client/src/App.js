5306c50d924723a4389f03df67d4545335edb7d9
Gerda Gatling <gerdagatling@Gerdas-MBP.home>
2020-01-29 13:51:32 -0500

    Add movie modal and updated handle funtions

--- myfavmovie_client/src/App.js
+++ myfavmovie_client/src/App.js
@@ -5,7 +5,7 @@ import NavigationBar from "./components/NavigationBar.js";
 import { Jumbotron } from "./components/Jumbotron.js";
 import Home from "./components/Home.js";
 import User from "./components/User.js"
-
+import SavedMovies from "./components/SavedMovies.js"
 
 class App extends Component{
 
@@ -23,6 +23,7 @@ class App extends Component{
             <Route exact path="/" component={Home} />
             <Route path="/home" component={Home} />
             <Route path="/user" component={User} />
+            <Route path="/savedmovies" component={SavedMovies} />
           </Switch>
         </Router>
 
--- myfavmovie_client/src/components/Movie.js
+++ myfavmovie_client/src/components/Movie.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {
+  Row, Col, Container
+} from 'react-bootstrap';
+
 
 const Movie = props => {
   return(
-    <div className="movies">
-      {props.title}
-    </div>
+    <Container className="allSneaker" style={
+      { padding: "5px",}
+    }>
+      <Row>
+        <Col style={{border: "1px solid black",boxShadow: "2px 2px #888888"}}>{props.title}</Col>
+        <img src =
+        {props.poster}
+        />
+      </Row>
+ </Container>
   )
 }
 
