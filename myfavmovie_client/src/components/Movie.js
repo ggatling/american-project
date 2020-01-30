04fd199894a6590bd4abbaad49dbfb976fdde321
Gerda Gatling <gerdagatling@Gerdas-MBP.home>
2020-01-29 23:44:11 -0500

    Refactored Movie component to return the poster within a card

--- myfavmovie_client/src/components/Movie.js
+++ myfavmovie_client/src/components/Movie.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import {
-  Row, Col, Container
-} from 'react-bootstrap';
+import {Card,Button} from 'react-bootstrap';
+
 
 
 const Movie = props => {
   return(
-    <Container className="allSneaker" style={
-      { padding: "5px",}
-    }>
-      <Row>
-        <Col style={{border: "1px solid black",boxShadow: "2px 2px #888888"}}>{props.title}</Col>
-        <img src =
-        {props.poster}
-        />
-      </Row>
- </Container>
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={props.poster} />
+    <Card.Body>
+      <Card.Title>{props.title}</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </Card.Text>
+        <Button variant="danger">Delete</Button>
+    </Card.Body>
+  </Card>
+
   )
 }
 
